af96272cd53e616b93aa0474cbada542e3471377
kiyoshion <kiyoshion@gmail.com>
2022-01-10 21:54:46 +0900

    Add layouts

---
+++ components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex justify-between">
+    <nuxt-link to="/">
+      LOGO
+    </nuxt-link>
+    <ul>
+      <li>
+        <nuxt-link to="/books">
+          BOOKS
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
