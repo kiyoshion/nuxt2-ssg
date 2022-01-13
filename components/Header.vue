29e55eb0f44ea555de519468aa53a3e0ca4cd92b
kiyoshion <kiyoshion@gmail.com>
2022-01-13 22:39:32 +0900

    Add words

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="flex justify-between">
+  <div class="container mx-auto flex justify-between">
     <nuxt-link to="/">
       LOGO
     </nuxt-link>
-    <ul>
+    <ul class="flex">
       <li>
         <nuxt-link to="/books">
           BOOKS
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/words">
+          WORDS
         </nuxt-link>
       </li>
     </ul>
